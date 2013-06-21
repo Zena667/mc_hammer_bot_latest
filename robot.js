6128c59a33ce35580aeb2a5a655899eb18d1cd15
Zena B <zena667@gmail.com>
2013-06-21 13:15:27 -0400

    engaged all motors on test server

--- robot.js
+++ robot.js
@@ -12,8 +12,7 @@ $(document).ready (function() {
 					// user presses the "Q" STRAFE LEFT
 					case 81: 	$(".Q").addClass("active");
 						$(document).keyup(function(e){
-						$.ajax({url: "http://localhost:8071/motion-control/update", data: {forward: 1}, dataType: "jsonp"});
-						  console.log("hello")
+						$.ajax({url: "http://localhost:8071/motion-control/update", data: {strafe: -1}, dataType: "jsonp"});
 						});	
 						$(".Q").removeClass("active");
 						
@@ -22,40 +21,45 @@ $(document).ready (function() {
 					// user presses the "W" key FORWARD
 					case 87:	$(".W").addClass("active");
 						$(document).keyup(function(e){
+						$.ajax({url: "http://localhost:8071/motion-control/update", data: {forward: 1}, dataType: "jsonp"});
+						});	
 						$(".W").removeClass("active");
-						});
 						
 						break;
 								
 			 		// user presses the "E" key STRAFE RIGHT
 					case 69:	$(".E").addClass("active");
 						$(document).keyup(function(e){
+						$.ajax({url: "http://localhost:8071/motion-control/update", data: {strafe: 1}, dataType: "jsonp"});
+						});	
 						$(".E").removeClass("active");
-						});
 						
 						break;
 								
 					// user presses the "A" key LEFT 
 					case 65:	$(".A").addClass("active");
 						$(document).keyup(function(e){
+						$.ajax({url: "http://localhost:8071/motion-control/update", data: {forward: -1}, dataType: "jsonp"});
+						});	
 						$(".A").removeClass("active");
-						});
 
 						break;
 								
 					// user presses the "S" key REVERSE 
 					case 83:	$(".S").addClass("active");
 						$(document).keyup(function(e){
+						$.ajax({url: "http://localhost:8071/motion-control/update", data: {strafe: -1}, dataType: "jsonp"});
+						});	
 						$(".S").removeClass("active");
-						});
 						
 						break; 
 
 					// user presses the "D" key RIGHT
 					case 68:	$(".D").addClass("active");
 						$(document).keyup(function(e){
+						$.ajax({url: "http://localhost:8071/motion-control/update", data: {turn: 1}, dataType: "jsonp"});
+						});	
 						$(".D").removeClass("active");
-						});
 						
 						break;
 
