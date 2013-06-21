2899a7ab6244586681725178f998fdf9f5dbb80d
Zena B <zena667@gmail.com>
2013-06-21 16:07:49 -0400

    key up/down function worked out

--- robot.js
+++ robot.js
@@ -1,91 +1,157 @@
-$(document).ready (function() {
-
-// GavinThompson
-
-
-
+$(document).ready(function() {
 	$(document).keydown(function(e) {
+		switch (e.which) {
+			// user presses the "Q" STRAFE LEFT
+			case 81:
+				$(".Q").addClass("active");
+				$.ajax({
+					url: "http://localhost:8071/motion-control/update",
+					data: {
+						strafe: -1
+					},
+					dataType: "jsonp"
+				});
+				break;
 
-		// listens for any navigation keypress activity
-			
-				switch(e.which) {
-					// user presses the "Q" STRAFE LEFT
-					case 81: 	$(".Q").addClass("active");
-						$(document).keyup(function(e){
-						$.ajax({url: "http://localhost:8071/motion-control/update", data: {strafe: -1}, dataType: "jsonp"});
-						});	
-						$(".Q").removeClass("active");
-						
-						break; 
-								
-					// user presses the "W" key FORWARD
-					case 87:	$(".W").addClass("active");
-						$(document).keyup(function(e){
-						$.ajax({url: "http://localhost:8071/motion-control/update", data: {forward: 1}, dataType: "jsonp"});
-						});	
-						$(".W").removeClass("active");
-						
-						break;
-								
-			 		// user presses the "E" key STRAFE RIGHT
-					case 69:	$(".E").addClass("active");
-						$(document).keyup(function(e){
-						$.ajax({url: "http://localhost:8071/motion-control/update", data: {strafe: 1}, dataType: "jsonp"});
-						});	
-						$(".E").removeClass("active");
-						
-						break;
-								
-					// user presses the "A" key LEFT 
-					case 65:	$(".A").addClass("active");
-						$(document).keyup(function(e){
-						$.ajax({url: "http://localhost:8071/motion-control/update", data: {forward: -1}, dataType: "jsonp"});
-						});	
-						$(".A").removeClass("active");
+				// user presses the "W" key FORWARD
+			case 87:
+				$(".W").addClass("active");
+				$.ajax({
+					url: "http://localhost:8071/motion-control/update",
+					data: {
+						forward: 1
+					},
+					dataType: "jsonp"
+				});
+				break;
 
-						break;
-								
-					// user presses the "S" key REVERSE 
-					case 83:	$(".S").addClass("active");
-						$(document).keyup(function(e){
-						$.ajax({url: "http://localhost:8071/motion-control/update", data: {strafe: -1}, dataType: "jsonp"});
-						});	
-						$(".S").removeClass("active");
-						
-						break; 
+				// user presses the "E" key STRAFE RIGHT
+			case 69:
+				$(".E").addClass("active");
+				$.ajax({
+					url: "http://localhost:8071/motion-control/update",
+					data: {
+						strafe: 1
+					},
+					dataType: "jsonp"
+				});
+				break;
 
-					// user presses the "D" key RIGHT
-					case 68:	$(".D").addClass("active");
-						$(document).keyup(function(e){
-						$.ajax({url: "http://localhost:8071/motion-control/update", data: {turn: 1}, dataType: "jsonp"});
-						});	
-						$(".D").removeClass("active");
-						
-						break;
+				// user presses the "A" key LEFT 
+			case 65:
+				$(".A").addClass("active");
+				$.ajax({
+					url: "http://localhost:8071/motion-control/update",
+					data: {
+						forward: -1
+					},
+					dataType: "jsonp"
+				});
+				break;
 
-								// user presses the "X" key STOP
-					case 88:	$(".X").addClass("active");
-						$(document).keyup(function(e){
-						$(".X").removeClass("active");
-						});
-						
-						break;
-				}
+				// user presses the "S" key REVERSE 
+			case 83:
+				$(".S").addClass("active");
+				$.ajax({
+					url: "http://localhost:8071/motion-control/update",
+					data: {
+						strafe: -1
+					},
+					dataType: "jsonp"
+				});
+				break;
+
+				// user presses the "D" key RIGHT
+			case 68:
+				$(".D").addClass("active");
+				$.ajax({
+					url: "http://localhost:8071/motion-control/update",
+					data: {
+						turn: 1
+					},
+					dataType: "jsonp"
+				});
+				break;
+		}
+	})
+	$(document).keyup(function(e) {
+
+		switch (e.which) {
+
+			case 81:
+				$(".Q").removeClass("active");
+				$.ajax({
+					url: "http://localhost:8071/motion-control/update",
+					data: {
+						strafe: 0
+					},
+					dataType: "jsonp"
+				});
+				break;
+
+				// user presses the "W" key FORWARD
+			case 87:
+				$(".W").removeClass("active");
+				$.ajax({
+					url: "http://localhost:8071/motion-control/update",
+					data: {
+						forward: 0
+					},
+					dataType: "jsonp"
+				});
+				break;
+
+				// user presses the "E" key STRAFE RIGHT
+			case 69:
+				$(".E").removeClass("active");
+				$.ajax({
+					url: "http://localhost:8071/motion-control/update",
+					data: {
+						strafe: 0
+					},
+					dataType: "jsonp"
+				});
+				break;
+
+				// user presses the "A" key LEFT 
+			case 65:
+				$(".A").removeClass("active");
+				$.ajax({
+					url: "http://localhost:8071/motion-control/update",
+					data: {
+						forward: 0
+					},
+					dataType: "jsonp"
+				});
+				break;
+
+				// user presses the "S" key REVERSE 
+			case 83:
+				$(".S").removeClass("active");
+				$.ajax({
+					url: "http://localhost:8071/motion-control/update",
+					data: {
+						strafe: -1
+					},
+					dataType: "jsonp"
+				});
+				break;
+
+				// user presses the "D" key RIGHT
+			case 68:
+				$(".D").removeClass("active");
+				$.ajax({
+					url: "http://localhost:8071/motion-control/update",
+					data: {
+						turn: 1
+					},
+					dataType: "jsonp"
+				});
+				break;
+		}
 	})
 });
-
-	
-
-
-// var test = $.post("http://localhost:8071/motors", function() {
-// 	alert("success");
-// })
-	
-
-
 
 
 // http://d2o0t5hpnwv4c1.cloudfront.net/013_keypress/demo/demo.html
 // http://net.tutsplus.com/tutorials/javascript-ajax/how-to-create-a-keypress-navigation-using-jquery/
-
-
